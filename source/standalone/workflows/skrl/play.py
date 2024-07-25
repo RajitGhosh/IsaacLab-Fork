e9029a2045b4e1f964dca7d9df20aff6dd71eb3f
Raj Ghosh <rajit.ghosh@lambdal.com>
2024-07-24 17:33:43 -0700

    made skrl model playbacks record a video

--- source/standalone/workflows/skrl/play.py
+++ source/standalone/workflows/skrl/play.py
@@ -26,19 +26,11 @@ parser.add_argument(
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
 parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint.")
-parser.add_argument(
-    "--ml_framework",
-    type=str,
-    default="torch",
-    choices=["torch", "jax", "jax-numpy"],
-    help="The ML framework used for training the skrl agent.",
-)
-
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
 args_cli = parser.parse_args()
-
+args_cli.enable_cameras = True
 # launch omniverse app
 app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
@@ -48,15 +40,12 @@ simulation_app = app_launcher.app
 import gymnasium as gym
 import os
 import torch
+from datetime import datetime
 
-import skrl
+from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.utils.model_instantiators.torch import deterministic_model, gaussian_model, shared_model
 
-if args_cli.ml_framework.startswith("torch"):
-    from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
-    from skrl.utils.model_instantiators.torch import deterministic_model, gaussian_model, shared_model
-elif args_cli.ml_framework.startswith("jax"):
-    from skrl.agents.jax.ppo import PPO, PPO_DEFAULT_CONFIG
-    from skrl.utils.model_instantiators.jax import deterministic_model, gaussian_model
+from omni.isaac.lab.utils.dict import print_dict
 
 import omni.isaac.lab_tasks  # noqa: F401
 from omni.isaac.lab_tasks.utils import get_checkpoint_path, load_cfg_from_registry, parse_env_cfg
@@ -65,38 +54,59 @@ from omni.isaac.lab_tasks.utils.wrappers.skrl import SkrlVecEnvWrapper, process_
 
 def main():
     """Play with skrl agent."""
-    # configure the ML framework into the global skrl variable
-    if args_cli.ml_framework.startswith("jax"):
-        skrl.config.jax.backend = "jax" if args_cli.ml_framework == "jax" else "numpy"
-    # parse configuration
+    # parse env configuration
     env_cfg = parse_env_cfg(
         args_cli.task, use_gpu=not args_cli.cpu, num_envs=args_cli.num_envs, use_fabric=not args_cli.disable_fabric
     )
     experiment_cfg = load_cfg_from_registry(args_cli.task, "skrl_cfg_entry_point")
 
+    # specify directory for logging experiments
+    task = experiment_cfg["agent"]["experiment"]["directory"]
+    log_root_path = os.path.join("logs", "skrl", task, "playing")
+    log_root_path = os.path.abspath(log_root_path)
+    print(f"[INFO] Logging experiment in directory: {log_root_path}")
+    # specify directory for logging runs: {time-stamp}_{run_name}
+    log_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if experiment_cfg["agent"]["experiment"]["experiment_name"]:
+        log_dir += f'_{experiment_cfg["agent"]["experiment"]["experiment_name"]}'
+    # set directory into agent config
+    experiment_cfg["agent"]["experiment"]["directory"] = log_root_path
+    experiment_cfg["agent"]["experiment"]["experiment_name"] = log_dir
+    # update log_dir
+    log_dir = os.path.join(log_root_path, log_dir)
+    
     # create isaac environment
-    env = gym.make(args_cli.task, cfg=env_cfg)
+    env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array")
+    
+    video_kwargs = {
+            "video_folder": os.path.join(log_dir, "videos"),
+            "episode_trigger": lambda episode: episode == 0,
+            "name_prefix": "skrl-" + task + "-" + experiment_cfg["agent"]["experiment"]["experiment_name"] + "-playing-video",
+            "disable_logger": True,
+        }
+    print("[INFO] Recording video of agent playing.")
+    print_dict(video_kwargs, nesting=4)
+    env = gym.wrappers.RecordVideo(env, **video_kwargs)
+    
     # wrap around environment for skrl
-    env = SkrlVecEnvWrapper(env, ml_framework=args_cli.ml_framework)  # same as: `wrap_env(env, wrapper="isaaclab")`
+    env = SkrlVecEnvWrapper(env)  # same as: `wrap_env(env, wrapper="isaaclab")`
 
     # instantiate models using skrl model instantiator utility
     # https://skrl.readthedocs.io/en/latest/api/utils/model_instantiators.html
     models = {}
-    if args_cli.ml_framework.startswith("jax"):
-        experiment_cfg["models"]["separate"] = True  # shared model is not supported in JAX
     # non-shared models
     if experiment_cfg["models"]["separate"]:
         models["policy"] = gaussian_model(
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device,
-            **process_skrl_cfg(experiment_cfg["models"]["policy"], ml_framework=args_cli.ml_framework),
+            **process_skrl_cfg(experiment_cfg["models"]["policy"]),
         )
         models["value"] = deterministic_model(
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device,
-            **process_skrl_cfg(experiment_cfg["models"]["value"], ml_framework=args_cli.ml_framework),
+            **process_skrl_cfg(experiment_cfg["models"]["value"]),
         )
     # shared models
     else:
@@ -107,21 +117,17 @@ def main():
             structure=None,
             roles=["policy", "value"],
             parameters=[
-                process_skrl_cfg(experiment_cfg["models"]["policy"], ml_framework=args_cli.ml_framework),
-                process_skrl_cfg(experiment_cfg["models"]["value"], ml_framework=args_cli.ml_framework),
+                process_skrl_cfg(experiment_cfg["models"]["policy"]),
+                process_skrl_cfg(experiment_cfg["models"]["value"]),
             ],
         )
         models["value"] = models["policy"]
-    # instantiate models' state dict
-    if args_cli.ml_framework.startswith("jax"):
-        for role, model in models.items():
-            model.init_state_dict(role)
 
     # configure and instantiate PPO agent
     # https://skrl.readthedocs.io/en/latest/api/agents/ppo.html
     agent_cfg = PPO_DEFAULT_CONFIG.copy()
     experiment_cfg["agent"]["rewards_shaper"] = None  # avoid 'dictionary changed size during iteration'
-    agent_cfg.update(process_skrl_cfg(experiment_cfg["agent"], ml_framework=args_cli.ml_framework))
+    agent_cfg.update(process_skrl_cfg(experiment_cfg["agent"]))
 
     agent_cfg["state_preprocessor_kwargs"].update({"size": env.observation_space, "device": env.device})
     agent_cfg["value_preprocessor_kwargs"].update({"size": 1, "device": env.device})
